b028c499e7f242778f864d2c3e1065de3d1b1a8e
SakibHasanCse <sakibhasan9641@gmail.com>
2021-05-01 22:20:56 +0600

    updateing

--- dynamicPrograming.js
+++ dynamicPrograming.js
@@ -19,11 +19,14 @@ const fib = (n, memo = {}) => {
 
 //gridTraveler
 
-const gridTraveler = (m, n) => {
+const gridTraveler = (m, n, memo = {}) => {
+    const key = m + ',' + n
+    if (key in mamo) return memo[key]
     if (m === 1 && n === 1) return 1;
     if (m === 0 || n === 0) return 0;
 
-    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+    memo[key] = gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+    return memo[key]
 
 
 }
---
+++ index2.js
@@ -0,0 +1,7 @@
+
+
+var aa = 100
+for (var i = 0; i < aa; i++) {
+    var n = "somrat"
+    console.log(n + " " + i)
+}
