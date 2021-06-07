e1f0b370756601f6024a48ec512796187a243156
SakibHasanCse <sakibhasan9641@gmail.com>
2021-06-07 10:58:49 +0600

    add

--- nodejs/index.js
+++ nodejs/index.js
@@ -15,4 +15,22 @@ function vowelsAndConsonants(s) {
     }
 }
 let s = 'javascriptloops'
-vowelsAndConsonants(s)
+// vowelsAndConsonants(s)
+
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+
+
+    return new RegExp(/^([aeiou]).*\1$/);
+
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+regexVar()
